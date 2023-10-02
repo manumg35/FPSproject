d6eba15064a801d5f11d4b20794dbc523c4e32fa
Manuel Martinez Giner <manumg35@outlook.es>
2023-10-02 22:01:10 +0200

    enemy AI prototype + standard assets

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    float distanceToTarget = Mathf.Infinity;
+    NavMeshAgent navMeshAgent;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(distanceToTarget<chaseRange)
+            navMeshAgent.SetDestination(target.position);
+    }
+}
